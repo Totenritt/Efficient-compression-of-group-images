b16eca7e7e942077bc6e03431317182e114458ec
lang16357 <128016290+lang16357@users.noreply.github.com>
2023-06-19 20:48:07 +1000

    Update encode&decode.py
    split the predict and encode

--- encode&decode.py
+++ encode&decode.py
@@ -5,7 +5,6 @@ import sys
 import imutils
 import glob
 import testset_build
-import mst
 import string 
 import re
 import subprocess
@@ -21,17 +20,28 @@ def findLayerThresholds(inputByte):
     threshold = re.sub(r'[^0-9,]', '', threshold)
     return threshold
 
-def encoder():
-    '''
-    encode the predictive image of child image with respect to the parent image
-    '''
-    #read the image
+def predict():
     parentName = input('input the name of parent image: ')
     childName = input('input the name of child image: ')
     parentImg = cv.imread('./data/' + parentName +'.jpeg')
     childImg = cv.imread('./data/' +childName +'.jpeg')
     #generate predicted image
-    predictImg = testset_build.generatePredictImg(parentImg,childImg, False, filename=childName)
+    predictImg = testset_build.generatePredictImgNew(parentImg,childImg, False, filename=childName)
+    return parentName, childName
+
+
+def encoder(parentName, childName):
+    '''
+    encode the predictive image of child image with respect to the parent image
+    '''
+    #read the image
+    childImg = cv.imread('./data/' +childName +'.jpeg')
+    #imread predicted image
+    predictImg = cv.imread('./data/'+childName+'_predicted.jpeg')
+    #imwrite predicted image.ppm and cpmpress it
+    cv.imwrite('./data/'+childName+'.ppm',predictImg)
+    compressCmd = "kdu_compress -i ./data/"+childName+".ppm -o ./data/"+childName+".jp2"
+    subprocess.run(compressCmd, shell=True)
     #generate residual image
     childImg = childImg.astype(int) #cast to int type to avoid negative values clipping 
     predictImg = predictImg.astype(int)
@@ -44,14 +54,18 @@ def encoder():
     thresholdCmd = "kdu_compress -i ./data/residual/offsetImg.ppm -o ./data/residual/offsetImg_output.jp2 Clayers=20"
     output = subprocess.check_output(thresholdCmd, shell=True)
     threshold = findLayerThresholds(output)
+    return threshold
+
+def decoder(threshold):
+
+    predictedName = input('input the name of predicted image: ')
+    decodedCmd = "kdu_expand -i ./data/"+predictedName+".jp2 -o ./data/"+predictedName+"_Decoded"+".ppm"
+    subprocess.run(decodedCmd, shell=True)
+    predictedImg = cv.imread("./data/"+predictedName+"_Decoded"+".ppm")
     # kakadu command for compress file 
     compressCmd = "kdu_compress -i ./data/residual/offsetImg.ppm -o ./data/residual/offsetImg_output.jp2 -slope " + threshold
     subprocess.run(compressCmd, shell=True)
-    return 0
-
-def decoder():
-    predictedName = input('input the name of predicted image: ')
-    predictedImg = cv.imread('./data/' + predictedName +'_predicted.jpeg')
+    
     for i in range(1,21):
         # kakadu command for expand file
         expandCmd = 'kdu_expand -i ./data/residual/offsetImg_output.jp2 -o ./data/residual/decodedImg_'+ str(i) +'.ppm -layers ' + str(i)
@@ -68,8 +82,9 @@ def decoder():
     return 0 
 
 if __name__ == '__main__':
-    encoder()
-    decoder()
+    a,b= predict()
+    c = encoder(a,b)
+    decoder(c)
 
 
 
